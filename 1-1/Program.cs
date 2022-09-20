809f6391f4ec93f6746d0c92d911e51d2707554a
Alexey Utkin <alex_habar@mail.ru>
2022-09-21 00:30:49 +0300

    1задача

---
+++ 1-1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число1: ");
+string input1 = Console.ReadLine();
+int a = int.Parse(input1);
+
+Console.WriteLine("Введите число2: ");
+string input2 = Console.ReadLine();
+int b = int.Parse(input2);
+
+if (a > b)
+{
+    Console.WriteLine("max " + a);
+    Console.WriteLine("min " + b);
+}
+else
+{
+    Console.WriteLine("max " + b);
+    Console.WriteLine("min " + a);
+}
