18acf321f830a8e59d6b6a6e1b7b7ffcf8237033
Alexey Utkin <alex_habar@mail.ru>
2022-09-21 01:27:49 +0300

    4задача

---
+++ 1-4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число1: ");
+string input1 = Console.ReadLine();
+int a = int.Parse(input1);
+int count = 2;
+while (count <= a)
+{
+    Console.WriteLine(count);
+    count += 2;
+}
