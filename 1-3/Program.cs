cbd44107ff8422c0b4fc220190d6afd862d9e8ff
Alexey Utkin <alex_habar@mail.ru>
2022-09-21 00:56:25 +0300

    3задача

---
+++ 1-3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+
+if (number % 2 == 0)
+{
+    Console.WriteLine ($"{number} Четное число");
+}
+else
+{
+    Console.WriteLine ($"{number} не четное число");
+}
